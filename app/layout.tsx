819f0c3789f02a4eab439af0daa2ec025b6d1608
jonideseta1-wq <jonideseta1@gmail.com>
2026-08-05 09:50:41 -0300

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "InmoGest",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
